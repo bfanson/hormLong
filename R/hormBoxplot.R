522a4674db38558d0a69737c03f3fc27ccaeeeab
bfanson <bfanson@gmail.com>
2014-12-31 19:05:52 +1100

    fix two issues 1) order of summary table 2) multiple plots for hormBoxplot

--- R/hormBoxplot.R
+++ R/hormBoxplot.R
@@ -2,16 +2,21 @@
 #' 
 #' @param data dataset [required]
 #' @param id_var name of the individual variable [required]
+#' @param by_var name of variable to break up plots by  [optional]
 #' @param conc_car name of the concentration variable [required]
 #' @param log_scale determines if y-axis is log10-scale or not. log-scale='y' makes log scale [default='n']  
+#' @param plot_per_page the number of plot panels per page, by row. [default = 4]
+#' @param plot_height  the height of individual plot panels (in inches).  Pdf page height is determined by both plot_per_page and plot_height. [default = 2]
+#' @param plot_width  the width of the pdf page. [default = 6]
 #' @return nothing...Produces a pdf file with the graph in current working directory
 #' @export
 #' @examples
 #' 
-#' hormBoxplot(data=hormone, conc_var='conc',id_var='id')
+#' hormBoxplot(data=hormone, conc_var='conc',id_var='id', by_var='sex')
 #' 
 
-hormBoxplot <- function(data, conc_var, id_var, plot_height=4, plot_width=6, save_plot=T, log_scale='n'){
+hormBoxplot <- function(data, conc_var, id_var, by_var, plot_height=5, plot_width=6, 
+                        save_plot=T, log_scale='n', plot_per_page=1){
   
   if(missing(conc_var)){
       stop('conc_var (e.g. the response variable containing the concentration) must be specified')
@@ -30,24 +35,33 @@ hormBoxplot <- function(data, conc_var, id_var, plot_height=4, plot_width=6, sav
   if(missing(id_var)){
       stop('id_var (e.g. animal name) must be specified')
     }
+  if(missing(by_var)){
+      by_var <- 'by'
+      data$by <- ' '   # create a by_variable so following code works
+    }
 
   if( save_plot ){
     pdf('hormBoxplot.pdf', height=plot_height, width = plot_width )
     cat( paste0('\n *********\nNote: plots are saved at: \n', getwd(),'/hormBoxplot.pdf \n***** \n\n')  )
   }
   
-  
-  par(mar=c(5,4,1,1))
-  data <- data[ order(data[,id_var]),]
-  if(log_scale=='y'){
-    h<-boxplot( data[,conc_var] ~ data[,id_var],ylab=conc_var, xaxt='n',log = 'y'  )
-  }else{
-    h<-boxplot( data[,conc_var] ~ data[,id_var],ylab=conc_var, xaxt='n'  )
-  }
-    points(as.numeric(as.factor(data[,id_var])), data[,conc_var] )
-    axis(1,at=1:length(h$names),labels=NA)
-    text(1:length(h$names), par("usr")[3] - (par("usr")[4]-par("usr")[3])*0.1 , srt = 315, adj = 0,  
-        labels = h$names, xpd = TRUE)
+  by_var_v <- cleanByvar(by_var ) 
+  data$plot_title <- getPlotTitle(data, by_var=by_var_v) 
+  par(mfrow=c(plot_per_page,1), mar=c(2,4,2,0.5),oma=c(2,2,2,1))
+  for(p in unique( data$plot_title ) ){
+    data1 <- subset(data, plot_title == p )
+    data1 <- data1[ order(data1[,id_var]),]
+    if(log_scale=='y'){
+      h<-boxplot( data1[,conc_var] ~ data1[,id_var],ylab=conc_var, xaxt='n',log = 'y'  )
+    }else{
+      h<-boxplot( data1[,conc_var] ~ data1[,id_var],ylab=conc_var, xaxt='n'  )
+    }
+      mtext(p, side = 3, cex=1.1)
+      points(as.numeric(as.factor(data1[,id_var])), data1[,conc_var] )
+      axis(1,at=1:length(h$names),labels=NA)
+      text(1:length(h$names), par("usr")[3] - (par("usr")[4]-par("usr")[3])*0.1 , srt = 315, adj = 0,  
+          labels = h$names, xpd = TRUE)
+  }  
   if( save_plot ){  dev.off() }
 }
 
--- R/hormSumTable.R
+++ R/hormSumTable.R
@@ -38,6 +38,8 @@ hormSumTable <- function(x,  num_decimals=2){
   ds2 <- merge(ds2,ds_b[,c(by_var_v, 'peak_base')],all=T)
 
   ds_out <- cbind( ds2[,by_var_v], sapply(ds2[,(length(by_var_v)+1):ncol(ds2)],round,num_decimals) )
+  ds_out <- ds_out[, c(by_var_v, 'mean','median','sd','percent_cv','min','max','cutoff','base_mean','peak_mean','peak_base')]
+
 
 #--- output table ---#
     write.csv(ds_out,file='hormSumTable.csv',quote=F, row.names=F,na='')
