04b683ed7db61b28986c45aff616fa479c581926
bfanson <bfanson@gmail.com>
2015-01-01 15:48:09 +1100

    added in hormElephant

---
+++ R/hormData.R
@@ -0,0 +1,12 @@
+#' Asian Elephant data
+#' 
+#' Longitudinal hormone data for multiple female elephants in which LH and Progesterone were   
+#' measured over years.
+#'  
+#' Reference: Fanson KV, Keeley T, Fanson BG. Cyclic changes in cortisol across the estrous 
+#' cycle in parous and nulliparous Asian elephants. Endocrine Connections 3:57-66 
+#' 
+#' @format A data frame with five variables: \code{ID}, \code{Sample_num}, 
+#'   \code{Date}, \code{Hormone}, \code{conc_ng_ml} and \code{Event}
+#'   
+"hormElephant"
