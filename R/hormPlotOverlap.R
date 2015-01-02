f2aa5ec771136a6c1a5aba71726825dbf7365377
bfanson <bfanson@gmail.com>
2015-01-02 14:31:20 +1100

    update hormPlotOverlap 1) has option of two_axes 2) color list is just a string, not a vector now

--- R/hormPlotOverlap.R
+++ R/hormPlotOverlap.R
@@ -2,7 +2,10 @@
 #' 
 #' @param x hormLong object (produced from hormBaseline) [required]
 #' @param hormone_var name for the hormone variable.  It must be listed in by_var during hormBaseline [required]
-#' @param colors vector of colors for the fills. It needs to have as many colors as hormone types  [default=c('red','blue')]
+#' @param colors list of colors for the fills. It needs to have as many colors as hormone types  [default='red,blue']
+#' @param add_fill should the area under the curve be filled with color (TRUE or FALSE) [default=TRUE]
+#' @param two_axes if TRUE then second axis will added to right side of plot.  Can only with two hormones
+#' If there are more than two hormones, function will give an error. [default=FALSE]
 #' @param date_format the format of the date variable on x-axis. See help for examples of other formats [default = '\%d-\%b']
 #' @param yscale  determines if y-axis should be free ('free') to change for each panel or remain the same ('fixed') for all panels [default = 'free']
 #' @param xscale  determines if x-axis should be free ('free') to change for each panel or remain the same ('fixed') for all panels  [default = 'free']
@@ -15,11 +18,18 @@
 #' @export
 #' @examples
 #' 
-#' result <- hormBaseline(data=hormLynx, criteria=2, by_var='AnimalID, Hormone', time_var='Date', conc_var='Conc' )
-#' hormPlotOverlap( result, hormone_var='Hormone', colors=c('red','blue','yellow') )
+#' result <- hormBaseline(data=hormLynx, criteria=2, by_var='AnimalID, Hormone', time_var='datetime', conc_var='Conc' )
+#' hormPlotOverlap( result, hormone_var='Hormone', colors='red, dark green, blue' )
+#' hormPlotOverlap( result, hormone_var='Hormone', colors='red, dark green, blue', add_fill=FALSE )
+#' hormPlotOverlap( result, hormone_var='Hormone', two_axes=T ) # this will produce an error
+#'
+#' result <- hormBaseline(data=hormElephant, criteria=2, by_var='ID, Hormone', time_var='Date', 
+#'              conc_var='conc_ng_ml' )
+#' hormPlotOverlap( result, hormone_var='Hormone', colors='red, dark green', two_axes=TRUE )
 #' 
 
-hormPlotOverlap <- function(x, hormone_var='horm_type', colors=c('red','blue'), date_format='%d-%b',
+hormPlotOverlap <- function(x, hormone_var='horm_type', colors='red,blue', date_format='%d-%b', 
+                     add_fill=TRUE, two_axes=FALSE,
                      xscale='free',yscale='free', 
                      plot_per_page=4, plot_height=2, plot_width=6, save_plot=TRUE,...){
   
@@ -44,7 +54,7 @@ hormPlotOverlap <- function(x, hormone_var='horm_type', colors=c('red','blue'), 
   if( !(xscale %in% c('free','fixed') ) ){
     stop( paste('xscale must be either "free" or "fixed", not:',xscale) )
   }
-  
+  colors <- cleanByvar(colors) # make a vector   
   
 #-- set-up ---#
   by_var_v <- cleanByvar(x$by_var) 
@@ -56,13 +66,20 @@ hormPlotOverlap <- function(x, hormone_var='horm_type', colors=c('red','blue'), 
   
   data$plot_title <- getPlotTitle(data, by_var=by_var_v)
 
+  #--- check the two hormone scenario ---#
+  if( two_axes ){ 
+    if( length(unique(data[,hormone_var]))!=2 ){
+      stop(paste0('Number of hormones must be two. Currently, there are ',length(unique(data[,hormone_var])) ) )
+    }
+  }
     
 #--- create plots ---#
   if( save_plot ){
     pdf('hormPlotOverlap.pdf', height=plot_per_page * plot_height, width = plot_width )
   }
 
-  par(mfrow=c(plot_per_page,1), mar=c(2,4,2,0.5),oma=c(2,2,2,1))
+  if( two_axes ){par(mfrow=c(plot_per_page,1), mar=c(2,4,2,4),oma=c(2,2,2,2))
+    }else{ par(mfrow=c(plot_per_page,1), mar=c(2,4,2,0.5),oma=c(2,2,2,0.5))}
   for( i in unique(data$plot_title) ){
     ds_sub <- data[data$plot_title==i, ]
     if(!is.null(x$event_var)){
@@ -85,6 +102,17 @@ hormPlotOverlap <- function(x, hormone_var='horm_type', colors=c('red','blue'), 
         xmin <- min( data[,time_var],na.rm=T)
         xmax <- max( data[,time_var],na.rm=T)
       }    
+      if( two_axes & yscale=='free' ){
+        ymin <- 0
+        ymax  <-  max(ds_sub[ ds_sub[hormone_var]==sort(unique(ds_sub[,hormone_var]))[1],conc_var],na.rm=T)*1.1
+        ymax2 <-  max(ds_sub[ ds_sub[hormone_var]==sort(unique(ds_sub[,hormone_var]))[2],conc_var],na.rm=T)*1.1
+      }  
+      if( two_axes & yscale=='fixed' ){
+        ymin <- 0
+        ymax  <-  max(data[ data[hormone_var]==sort(unique(data[,hormone_var]))[1],conc_var],na.rm=T)*1.1
+        ymax2 <-  max(data[ data[hormone_var]==sort(unique(data[,hormone_var]))[2],conc_var],na.rm=T)*1.1
+      }  
+    
     #--- main plot
       require(lubridate)
       loop <- 0
@@ -92,12 +120,22 @@ hormPlotOverlap <- function(x, hormone_var='horm_type', colors=c('red','blue'), 
         loop <- loop+1
         ds_sub1 <- ds_sub[ds_sub[,hormone_var]==h,]
         if(loop==1){
-          plot(ds_sub1[,conc_var] ~ ds_sub1[,time_var], type='l',xlim=c(xmin,xmax), ylim=c(ymin,ymax), 
-            xlab=NA, ylab=conc_var, xaxt='n' )
-        }else{ lines(ds_sub1[,time_var],ds_sub1[,conc_var] )}
+          y_lab=conc_var
+          if( two_axes ){ y_lab <- h}
+           plot(ds_sub1[,conc_var] ~ ds_sub1[,time_var], type='l',xlim=c(xmin,xmax), ylim=c(ymin,ymax), 
+            xlab=NA, ylab=y_lab, xaxt='n', col=colors[loop] )
+        }
+        if(loop>1 & two_axes==FALSE){lines(ds_sub1[,time_var],ds_sub1[,conc_var],col=colors[loop] )}
+        if(loop>1 & two_axes){
+          par( new=T )
+          plot(ds_sub1[,conc_var] ~ ds_sub1[,time_var], type='l',xlim=c(xmin,xmax), ylim=c(ymin,ymax2), 
+            xlab=NA, ylab=NA, xaxt='n', yaxt='n', col=colors[loop] )
+          axis(4)
+          mtext(h,4,line=2.5)
+        }
         t_order <- c(ds_sub1[,time_var], rev(ds_sub1[,time_var]) )
         c_order <- c(rep(0,length(ds_sub1[,conc_var])),rev(ds_sub1[,conc_var])) 
-        polygon(t_order,c_order,col=adjustcolor(colors[loop], alpha=0.25)) 
+        if( add_fill) polygon(t_order,c_order,col=adjustcolor(colors[loop], alpha=0.25),border=colors[loop]) 
       }
       legend('topleft',legend=sort(unique(ds_sub[,hormone_var])),fill=adjustcolor(colors, alpha=0.25),
               bty='n',cex=0.9,bg=NA, pt.cex=0.6)
