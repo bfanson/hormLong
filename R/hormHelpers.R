b96c4e7da9f0774cfff8097d9368895a55cbf73d
bfanson <bfanson@gmail.com>
2015-03-08 12:31:32 +1100

    changed choose.files() to file.choose 1) goal was to make compatible on a mac

--- R/hormHelpers.R
+++ R/hormHelpers.R
@@ -80,7 +80,7 @@ hormDate <- function(data, date_var, time_var, name='datetime', date_order='dmy'
 #' ds <- hormRead()
 
 hormRead <- function( ){
-  ds<- read.csv(choose.files(caption='Select hormone data file') ) 
+  ds<- read.csv(file.choose() ) 
   cat('Below shows the first 6 lines of the imported dataset.  check that it imported correctly.
       If not, check help file.\n\n')
   print( head(ds) )
